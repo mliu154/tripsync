51537c5d65bb767cbdf885ea34552b19b64b4247
mliu154 <mliu154@myseneca.ca>
2026-06-16 11:30:55 -0400

    Initial commit with code to close modals

--- C:\Users\Meng Liu\Desktop\tripsync/.next/dev/static/chunks/12t3_next_dist_client_components_builtin_global-error_0mbsxhv.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/12t3_next_dist_0u-n147._.js"],
-    source: "dynamic"
-});
